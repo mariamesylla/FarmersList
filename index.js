d2572a3d36b1c3a843ea0e355f545deb79121cf4
Andrew <andrewozo123@gmail.com>
2023-11-06 16:07:28 -0500

    finished customer class

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
 // Import Classes Here
-const Product = require('./classes/Product');
+const Product = require("./classes/Product");
 
-const Cart=require('./classes/Cart')
+const Cart = require("./classes/Cart");
 
-
-
-
-
-
-
-
-
+const Customer = require("./classes/Customer");
 
 // DO NOT EDIT BELOW THIS LINE
 try {
-    module.exports = {
-        Product,
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart,
-        Customer
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+    Customer,
+  };
+} catch (e) {}
 
 try {
-    module.exports = {
-        Product,
-        Cart,
-        Customer,
-        Auth
-    }
-} catch(e){
-
-}
+  module.exports = {
+    Product,
+    Cart,
+    Customer,
+    Auth,
+  };
+} catch (e) {}
